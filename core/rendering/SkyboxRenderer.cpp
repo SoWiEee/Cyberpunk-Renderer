aa13a709e631fae9f15c0bdb44740eaba66b0981
SoWiEee <kawiisowieee@gmail.com>
2025-11-24 09:30:46 +0800

    Fix color space RGBA

--- core/rendering/SkyboxRenderer.cpp
+++ core/rendering/SkyboxRenderer.cpp
@@ -106,8 +106,8 @@ unsigned int SkyboxRenderer::loadCubemap(std::vector<std::string> faces) {
     for (unsigned int i = 0; i < faces.size(); i++) {
         unsigned char* data = stbi_load(faces[i].c_str(), &width, &height, &nrChannels, 0);
         if (data) {
-            // 這裡假設圖片是 RGB
-            glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+            // RGBA
+            glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
             stbi_image_free(data);
         }
         else {
